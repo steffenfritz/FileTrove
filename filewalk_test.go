83d36977f9aa243f04c2d835805bdce4079bd3f8
Steffen Fritz <steffen@fritz.wtf>
2023-11-02 22:45:55 +0100

    added support for Dublin Core for sessions via a JSON file

--- filewalk_test.go
+++ filewalk_test.go
@@ -17,9 +17,8 @@ func TestCreateFileList(t *testing.T) {
 		wantErr bool
 	}{
 		{"testdata file list", args{"testdata"}, []string{"testdata/.hiddenfile",
-			"testdata/directory/A_PDF_File.pdf", "testdata/emptyfile.txt", "testdata/noaccess.rtf",
-			"testdata/textfile.txt", "testdata/transparent.png", "testdata/white.jpg"},
-			[]string{"testdata", "testdata/.hiddendir", "testdata/directory"}, false},
+			"testdata/directory/A_PDF_File.pdf", "testdata/emptyfile.txt", "testdata/images/screenshot_1.jpeg", "testdata/images/screenshot_1.jpeg_original", "testdata/images/screenshot_1.png", "testdata/images/screenshot_1.png_original", "testdata/images/screenshot_1.tiff", "testdata/images/screenshot_1.tiff_original", "testdata/noaccess.rtf", "testdata/textfile.txt", "testdata/transparent.png", "testdata/white.jpg"},
+			[]string{"testdata", "testdata/.hiddendir", "testdata/directory", "testdata/images"}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
