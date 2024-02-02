62de22ce034b375d0b1b8c9fdc93f7828be7ec43
Steffen Fritz <steffen@fritz.wtf>
2024-02-02 11:41:19 +0100

    update admftrove

--- cmd/admftrove/main.go
+++ cmd/admftrove/main.go
@@ -111,5 +111,22 @@ func main() {
 			return
 		}
 
+		// Update version 1.0.0-DEV-9 --> 1.0.0-DEV-10
+		if instversion == "1.0.0-DEV-9" {
+			_, err = ftdb.Exec("UPDATE filetrove SET version = '1.0.0-DEV-10' where version = '1.0.0-DEV-9'")
+			if err != nil {
+				logger.Error("Could not update database", slog.String("error", err.Error()))
+				return
+			}
+			updatetime := time.Now().Format(time.RFC3339)
+			_, err = ftdb.Exec("UPDATE filetrove SET lastupdate = ?", updatetime)
+			if err != nil {
+				logger.Error("Could not update last update time.", slog.String("error", err.Error()))
+				return
+			}
+			logger.Info("FileTrove database updated to version 1.0.0-DEV-10.")
+			return
+		}
+
 	}
 }
